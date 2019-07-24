beb442f31a145046be0aa4cf5671ff9e9183b89b
zhuyeqi <947996900@qq.com>
2019-07-24 21:48:58 +0800

    增加部分页面相关的controller

--- src/main/java/com/neuedu/project/controller/PageController.java
+++ src/main/java/com/neuedu/project/controller/PageController.java
@@ -7,9 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping(value = "hello")
-    public String hello() {
-        return "hello.html";
+    @GetMapping(value = "/student")
+    public String studentPage() {
+        // todo 需要拦截器
+        return "student.html";
+    }
+
+    @GetMapping(value = "/teacher")
+    public String teacherPage() {
+        // todo 需要拦截器
+        return "teacher.html";
+    }
+
+    @GetMapping(value = "/admin")
+    public String admin() {
+        return "admin.html";
     }
 
 }

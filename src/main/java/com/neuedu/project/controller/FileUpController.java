a1a7faad5c8ed80982b363e1a277c753770a606d
1173710230 <1250366059@qq.com>
2019-07-24 19:43:30 +0800

    FileUp Commit

---
+++ src/main/java/com/neuedu/project/controller/FileUpController.java
@@ -0,0 +1,40 @@
+package com.neuedu.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+public class FileUpController {
+
+    //fileUp
+    @RequestMapping(value = "fileUp",method = RequestMethod.POST)
+    public String up(MultipartFile img){
+        System.out.println(img);
+        //img  ----服务器的D://images   ---虚拟目录
+        if(img!=null) {
+            String fileName = img.getOriginalFilename();
+            System.out.println("上传的文件名字："+fileName);
+            //xxxseconds.xxx
+            int index = fileName.indexOf('.');
+            String newFileName = fileName.substring(0, index)+System.currentTimeMillis()+fileName.substring(index);
+            File file = new File("/Users/anothertask/Downloads/images",newFileName);
+            try {
+                img.transferTo(file);
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+        return "redirect:hello.html";
+    }
+}

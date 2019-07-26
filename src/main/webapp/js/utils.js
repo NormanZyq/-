86f4cc8ddc311cfac09546bf089de5137f533e20
zhuyeqi <947996900@qq.com>
2019-07-26 09:50:06 +0800

    加了一个style.css，里面的样式img-rd-lg和img-rd-md很好用

--- src/main/webapp/js/utils.js
+++ src/main/webapp/js/utils.js
@@ -6,22 +6,3 @@ function navBar() {
 
 }
 
-function login(id, password) {
-    $.ajax({
-        url: "/user/login",
-        dataType: "text",
-        method: "POST",
-        data: {
-            userId: id,
-            password: password
-        },
-        success: function (result) {
-            if (result == 'ok') {
-                alert("OK!!!!");
-                userId = id;
-            } else {
-                alert("失败");
-            }
-        }
-    })
-}
